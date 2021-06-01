ddd67c728a3a999a394d4908d7b415801ae0c05f
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-01 12:45:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
- 
+
 app.get('/hello', function (req, res) {
   res.send('Hello World')
 })
@@ -8,5 +8,5 @@ app.get('/hello', function (req, res) {
 app.get('/mensagens', function (req, res) {
     res.send('Exibir todas as mensagens.')
 })
- 
+
 app.listen(3000)
