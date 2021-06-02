1d2b3e535a3ce5c8ea7d3ee69db6c7b8ac3d45bb
Paulo Salvatore <salvatoregames@gmail.com>
2021-06-02 11:12:36 -0300

    Endpoint read all, read single e formatação do arquivo com o prettier

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/hello', function (req, res) {
-  res.send('Hello, World!')
-})
+app.get("/hello", function (req, res) {
+    res.send("Hello, World!");
+});
 
-app.get('/mensagens', function (req, res) {
-    res.send('Exibir todas as mensagens.')
-})
+const mensagens = ["Essa é a primeira mensagem", "Essa é a segunda mensagem"];
+//                            0                               1
 
-app.listen(3000)
+// Read All (Ler todas as mensagens)
+app.get("/mensagens", function (req, res) {
+    res.send(mensagens);
+});
+
+// Read Single (Ler apenas uma mensagem)
+app.get("/mensagens/:id", function (req, res) {
+    const id = req.params.id - 1;
+
+    const mensagem = mensagens[id];
+
+    res.send({ mensagem });
+});
+
+app.listen(3000);
